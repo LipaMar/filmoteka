63f4a3036189604ef00d6c9987da66c96c6d0b7b
Marcin <LipaMar>
2020-09-16 09:48:50 +0200

    many to many relationship user-review

--- src/main/java/lipamar/filmoteka/data/entities/Review.java
+++ src/main/java/lipamar/filmoteka/data/entities/Review.java
@@ -1,6 +1,10 @@
 package lipamar.filmoteka.data.entities;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "Reviews")
@@ -10,9 +14,56 @@ public class Review {
     private Long id;
     @Column
     private String content;
-    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private User author;
-
+    @Column
+    private String movieID;
+    @Column
+    private Date date;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "User_Review",
+            joinColumns = { @JoinColumn(name = "Reviews.id") },
+            inverseJoinColumns = { @JoinColumn(name = "Users.id") }
+    )
+    private Set<User> userLiked = new HashSet<>();
     public Review() {
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getMovieID() {
+        return movieID;
+    }
+
+    public void setMovieID(String movieID) {
+        this.movieID = movieID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
--- src/main/java/lipamar/filmoteka/data/entities/User.java
+++ src/main/java/lipamar/filmoteka/data/entities/User.java
@@ -1,6 +1,8 @@
 package lipamar.filmoteka.data.entities;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "Users")
@@ -12,6 +14,8 @@ public class User {
     private String username;
     @Column
     private String password;
+    @ManyToMany(mappedBy = "userLiked")
+    private Set<Review> likedReviews = new HashSet<>();
 
     public User() {
     }
