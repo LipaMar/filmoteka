9687bac4b5202963616c13904193e14629273c3e
LipaMar <marcin.lipa@onet.pl>
2020-09-17 17:49:14 +0200

    register validation

--- src/main/java/lipamar/filmoteka/data/entities/User.java
+++ src/main/java/lipamar/filmoteka/data/entities/User.java
@@ -1,6 +1,12 @@
 package lipamar.filmoteka.data.entities;
 
+import org.springframework.validation.annotation.Validated;
+
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,8 +17,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
+    @Pattern(regexp = "[a-zA-z0-9]{4,15}",message = "Username must be 4 to 15 characters long and must only contain letters and numbers")
     private String username;
     @Column
+    @Size(min = 5,max = 15,message = "Password must be 5 to 15 characters long")
     private String password;
     @ManyToMany(mappedBy = "userLiked")
     private final Set<Review> likedReviews = new HashSet<>();
--- src/main/java/lipamar/filmoteka/domain/controller/RegisterController.java
+++ src/main/java/lipamar/filmoteka/domain/controller/RegisterController.java
@@ -6,12 +6,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/register")
@@ -31,7 +33,10 @@ public class RegisterController {
     }
 
     @PostMapping
-    public String register(User user, HttpServletRequest request) {
+    public String register(@Valid User user, Errors errors, HttpServletRequest request) {
+        if(errors.hasErrors()){
+            return "register";
+        }
         repository.save(user);
         try {
             request.login(user.getUsername(), user.getPassword());
