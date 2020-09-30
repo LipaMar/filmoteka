a227edf1aee8fe5465a07dac62e0700d9cbc397a
LipaMar <marcin.lipa@onet.pl>
2020-09-30 19:41:54 +0200

    add page with follows listed

--- src/main/java/lipamar/filmoteka/domain/follow/FollowRepository.java
+++ src/main/java/lipamar/filmoteka/domain/follow/FollowRepository.java
@@ -4,7 +4,10 @@ import lipamar.filmoteka.domain.user.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface FollowRepository extends CrudRepository<Follow, Long>{
     Follow findByFollowerAndMovieId(User user, String movieId);
+    Set<Follow> findAllByFollower(User user);
 }
